ced233c0bdfa9848c85712457e4421956af8b4b3
spark (kukulam) <kukula.milosz@gmail.com>
2019-04-03 20:45:07 +0200

    Auto commit.

--- install_env.sh
+++ install_env.sh
@@ -1,94 +1,91 @@
 #!/bin/bash
-
-set -e
-
-sudo apt install tmux
-sudo apt install git
-sudo apt install kpcli
-sudo apt install snapd
-sudo aptitude install wmctrl  
-sudo apt install vim-gtk3
-sudo apt install trash-cli
-sudo apt install build-essential cmake python3-dev
-
-sudo add-apt-repository ppa:graphics-drivers
-sudo apt update
-sudo apt install nvidia-390
-
-# zsh
-sudo apt install zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# java 11
-sudo add-apt-repository ppa:linuxuprising/java
-sudo apt update
-sudo apt install oracle-java11-installer
-
-# kotlin
-snap install --classic kotlin
-
-# gradle
-sudo add-apt-repository ppa:cwchien/gradle
-sudo apt-get update
-sudo apt install gradle
-
-# node 11
-sudo apt-get install curl python-software-properties
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt-get install nodejs
-
-# google-chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-
-# spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install spotify-client
-
-# unetbootin
-sudo add-apt-repository ppa:gezakovacs/ppa
-sudo apt  update
-sudo apt install unetbootin
-
-# virtualbox
-sudo apt install virtualbox 
-
-# vsc
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install code # or code-insiders
-
-# pycharm community
-sudo snap install pycharm-community --classic
-
-# intelij-community
-sudo snap install intellij-idea-community --classic
-
-# intelij-ultimate
-# sudo snap install intellij-idea-ultimate --classic
-
-# docker community
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
-
-# rawtherapee
-sudo add-apt-repository ppa:dhor/myway
-sudo apt-get update
-sudo apt-get install rawtherapee
-
-
-
+#
+#set -e
+#
+#sudo apt install tmux
+#sudo apt install git
+#sudo apt install kpcli
+#sudo apt install snapd
+#sudo aptitude install wmctrl  
+#sudo apt install vim-gtk3
+#sudo apt install trash-cli
+#sudo apt install build-essential cmake python3-dev
+#
+#sudo add-apt-repository ppa:graphics-drivers
+#sudo apt update
+#sudo apt install nvidia-390
+#
+## zsh
+#sudo apt install zsh
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#
+## java 11
+#sudo add-apt-repository ppa:linuxuprising/java
+#sudo apt update
+#sudo apt install oracle-java11-installer
+#
+## kotlin
+#snap install --classic kotlin
+#
+## gradle
+#sudo add-apt-repository ppa:cwchien/gradle
+#sudo apt-get update
+#sudo apt install gradle
+#
+## node 11
+#sudo apt-get install curl python-software-properties
+#curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+#sudo apt-get install nodejs
+#
+## google-chrome
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+#rm google-chrome-stable_current_amd64.deb
+#
+## spotify
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+#echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+#sudo apt-get update
+#sudo apt-get install spotify-client
+#
+## unetbootin
+#sudo add-apt-repository ppa:gezakovacs/ppa
+#sudo apt  update
+#sudo apt install unetbootin
+#
+## virtualbox
+#sudo apt install virtualbox 
+#
+## vsc
+#curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+#sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+#sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+#sudo apt-get install apt-transport-https
+#sudo apt-get update
+#sudo apt-get install code # or code-insiders
+#
+## pycharm community
+#sudo snap install pycharm-community --classic
+#
+## intelij-community
+#sudo snap install intellij-idea-community --classic
+#
+## intelij-ultimate
+## sudo snap install intellij-idea-ultimate --classic
+#
+## docker community
+#sudo apt install apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+#sudo apt update
+#apt-cache policy docker-ce
+#sudo apt install docker-ce
+#sudo systemctl status docker
+#
+## rawtherapee
+#sudo add-apt-repository ppa:dhor/myway
+#sudo apt-get update
+#sudo apt-get install rawtherapee
 
 
 
